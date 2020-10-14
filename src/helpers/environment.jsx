12c486ebc0a5c457f8484c39605da3ba0ba11ae9
Jeremy <jbvidal101@gmail.com>
2020-10-14 13:59:24 -0400

    fixed the url

--- src/helpers/environment.jsx
+++ src/helpers/environment.jsx
@@ -1,12 +1,12 @@
 let APIURL = "";
 
-// console.log(window.location.hostname);
+console.log(window.location.hostname);
 switch (window.location.hostname) {
   case "localhost" || "127.0.0.1":
     APIURL = "http://localhost:3000";
     break;
 
-  case "jbv-mybudget-tracker.herokuapp.com":
+  case "jbv-mybudget-tracker-client.herokuapp.com":
 	APIURL = "https://jbv-mybudget-tracker.herokuapp.com";
     break;
 	
