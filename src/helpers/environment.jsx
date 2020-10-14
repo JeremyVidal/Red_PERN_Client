3068227446fcdf050fedab9b6c386c88b1726ec1
Jeremy <jbvidal101@gmail.com>
2020-10-14 14:06:15 -0400

    trying to fix url issue

--- src/helpers/environment.jsx
+++ src/helpers/environment.jsx
@@ -1,6 +1,6 @@
 let APIURL = "";
 
-console.log(window.location.hostname);
+console.log(`HOST NAME: ${window.location.hostname}`);
 switch (window.location.hostname) {
   case "localhost" || "127.0.0.1":
     APIURL = "http://localhost:3000";
